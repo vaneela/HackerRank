df800734427888e3024b9906376eab423a3611ec
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-04-22 21:40:11 +0530

    22-04-2022

---
+++ Migratory_Birds.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    map<int, int> birds;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        birds[x]++;
+    }
+    auto it = birds.begin();
+    int max = it->second, maxid = it->first;
+    for (auto b : birds)
+    {
+        if (b.second > max)
+        {
+            max = b.second;
+            maxid = b.first;
+        }
+    }
+    cout << maxid << endl;
+
+    return 0;
+}
