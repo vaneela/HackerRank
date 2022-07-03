47beb86dc982359b4b6ce56fa2f407e68cfd2c20
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-07-03 17:34:14 +0530

    3-07-2022

--- Circular_array_rotation.cpp
+++ Circular_array_rotation.cpp
@@ -6,16 +6,6 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
-/*
- * Complete the 'circularArrayRotation' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER k
- *  3. INTEGER_ARRAY queries
- */
-
 vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries) {
     vector<int>ans;
     k%=a.size();
